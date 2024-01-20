c778d918f0507df17142bf8a1f4f1760c468ccc4
Giuseppe <giuseppe.ferrarini@gmail.com>
2024-01-20 10:40:33 -0300

    little refactor of the solution

--- Solution.php
+++ Solution.php
@@ -1,4 +1,5 @@
 class Solution {
+    const MAX_INT = 2147483647;
     
     /**
      * @param Integer[][] $matrix
@@ -8,19 +9,13 @@ class Solution {
         $n = count($matrix);
         $dp = array_fill(0, $n, array_fill(0,$n,0));
 
-        for($i = 0; $i < $n; $i++){
+        for($i = 0; $i < $n; $i++) {
             $dp[0][$i] = $matrix[0][$i];
         }
 
         for($i = 1; $i < $n; $i++) {
             for($j = 0; $j < $n; $j++) {
-                if ($j == 0) {
-                    $dp[$i][$j] = $matrix[$i][$j] + min([$dp[$i-1][$j], $dp[$i-1][$j+1]]);
-                } else if ($j == $n-1) {
-                    $dp[$i][$j] = $matrix[$i][$j] + min([$dp[$i-1][$j], $dp[$i-1][$j-1]]);
-                } else {
-                    $dp[$i][$j] = $matrix[$i][$j] + min([$dp[$i-1][$j], $dp[$i-1][$j+1], $dp[$i-1][$j-1]]);
-                }
+                $dp[$i][$j] = $matrix[$i][$j] + min([$dp[$i-1][$j]??self::MAX_INT, $dp[$i-1][$j+1]??self::MAX_INT, $dp[$i-1][$j-1]??self::MAX_INT]);
             }
         }
         
